bf523542de18a86122751c3eb28e24d2c81f2280
chandan my <chandanchandumy@gmail.com>
2023-10-25 16:50:21 +0530

    first commit

---
+++ 25-10-2023/xammp.php
@@ -0,0 +1,17 @@
+<?php
+$name="hello world";
+$age=13;
+$b=4.0;
+$type=true;
+echo $name;
+var_dump($name);
+echo "<br>";
+print $age;
+var_dump ($age);
+echo "<br>";
+echo $type;
+var_dump ($type);
+echo "<br>";
+print $b;
+var_dump ($b);
+?>
