0c44d5126894220da1f5f821b3df48eec7299e76
chandan my <chandanchandumy@gmail.com>
2023-10-26 16:22:59 +0530

    26-10-2023

---
+++ 26-10-2023/comment.php
@@ -0,0 +1,17 @@
+<?php
+$a="tecnerds";
+//$name="hello world";
+#$age=13;
+/*$b=4.0;
+$type=true;
+echo $name;*/
+echo "<br>";
+print $age;
+echo "<br>";
+echo $type;
+echo "<br>";
+print $a;
+echo $a;
+print "technerds"."<br>".$a;
+echo("technerds"."<br>".$a);
+?>
